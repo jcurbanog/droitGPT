c542cc3e28fc9d83e99c41a3463c07ba43818f11
jurbanog <=>
2024-01-26 12:05:22 +0100

    message input ok

--- app/src/app/home/chat/chat.component.ts
+++ app/src/app/home/chat/chat.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component } from '@angular/core'
+
+import { MessagesComponent } from './messages/messages.component'
+import { InputMessageComponent } from './input-message/input-message.component'
 
 @Component({
-  selector: 'app-chat',
-  standalone: true,
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+	selector: 'app-chat',
+	standalone: true,
+	templateUrl: './chat.component.html',
+	styleUrl: './chat.component.scss',
+	imports: [CommonModule, MessagesComponent, InputMessageComponent],
 })
-export class ChatComponent {
-
-}
+export class ChatComponent {}
