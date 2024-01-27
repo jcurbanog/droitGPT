dd9ab3f19b241623ca582e94f6fe3e297f5297bc
jurbanog <=>
2024-01-27 17:48:48 +0100

    use all endpoints

--- app/src/app/home/chat/chat.component.ts
+++ app/src/app/home/chat/chat.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common'
-import { Component } from '@angular/core'
-import { FormControl } from '@angular/forms'
+import { Component, Input } from '@angular/core'
+import { FormGroup } from '@angular/forms'
 
 import { Message, MessagesComponent } from './messages/messages.component'
 import { InputMessageComponent } from './input-message/input-message.component'
 
 import { ModelQueryService, Response } from '../../services/model-query.service'
+import { QueryForm } from '../home.component'
 
 @Component({
 	selector: 'app-chat',
@@ -15,17 +16,22 @@ import { ModelQueryService, Response } from '../../services/model-query.service'
 	imports: [CommonModule, MessagesComponent, InputMessageComponent],
 })
 export class ChatComponent {
-	protected inputForm = new FormControl<string | null>('')
+	@Input({ required: true }) public form!: FormGroup<QueryForm>
+
 	protected messages: Message[] = []
 
 	constructor(private modelService: ModelQueryService) {}
 
 	protected async sendMessage(): Promise<void> {
-		const query = this.inputForm.value
+		const query = this.form.controls.input.value
 		if (query) {
-			this.inputForm.setValue('')
+			this.form.controls.input.setValue('')
 			this.messages.push({ text: query, speaker: 'user' })
-			const response: Response = await this.modelService.singleResponse(query, this.messages.slice(0, -1))
+			const response: Response = await this.modelService.request(
+				`${this.form.controls.mode.value.toLowerCase()}_response`,
+				query,
+				this.messages.slice(0, -1)
+			)
 			if (response.response.length) {
 				this.messages.push({ text: response.response[0], speaker: 'bot' })
 			}
